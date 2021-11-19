f7f51f0fad21b853821d99ad23f89aec54ac1bad
Momin2004 <m.musa@student.avans.nl>
2021-11-19 20:48:54 +0100

    Update Buzzer.java

--- src/Buzzer.java
+++ src/Buzzer.java
@@ -5,20 +5,42 @@ public class Buzzer {
     private int frequency;
     private int time;
 
+    /**
+     *
+     * @param outputPin
+     * @param frequency
+     * @param time
+     */
+
+
     public Buzzer(int outputPin, int frequency, int time) {
         this.frequency = frequency;
         this.outputPin = outputPin;
-        this.time = time; //time in milliseconds
+        this.time = time;
+
     }
+
+    /**
+     *
+     * Get variable values from 'outputPin, frequency, time'
+     * Call freqOut method
+     */
 
     public void start(){
         BoeBot.freqOut(this.outputPin, this.frequency, this.time);
-        //turn buzzer on for specific amount of time
+
     }
+
+    /**
+     *
+     * @param outputPin
+     * @param frequency
+     * @param time
+     */
     public void update(int outputPin, int frequency, int time){
         this.frequency = frequency;
         this.outputPin = outputPin;
         this.time = time;
-        //change the data of buzzer
+
     }
 }
