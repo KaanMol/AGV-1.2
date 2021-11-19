88eba9a2d852e5d58514de4cef54fd47661d313c
Kaan <m.mollaoglu@student.avans.nl>
2021-11-19 21:50:53 +0100

    Moved the Movementclass to the package vehicle

--- src/vehicle/Movement.java
+++ src/vehicle/Movement.java
@@ -1,32 +1,36 @@
-public class Movement {
+package vehicle;
+
+import hardware.Servo;
+import interfaces.Updatable;
+import common.Config;
+
+public class Movement implements Updatable {
 
     private Servo rightServo;
     private Servo leftServo;
 
-    Movement() {
-        this.rightServo = new Servo(Config.rightServoPin, false);
-        this.leftServo = new Servo(Config.leftServoPin, false);
+    public Movement() {
+        this.rightServo = new Servo(Config.rightServoPin);
+        this.leftServo = new Servo(Config.leftServoPin);
     }
 
     public void forward() {
         this.rightServo.setSpeed(1300);
         this.leftServo.setSpeed(1700);
-        this.leftServo.start();
-        this.rightServo.start();
     }
 
     public void backwards() {
         this.leftServo.setSpeed(1300);
         this.rightServo.setSpeed(1700);
-        this.leftServo.start();
-        this.rightServo.start();
     }
 
     public void turnRight() {
         this.leftServo.setSpeed(1300);
         this.rightServo.setSpeed(1700);
-        this.leftServo.start();
-        this.rightServo.start();
+    }
+
+    public void update() {
+
     }
 
 }
