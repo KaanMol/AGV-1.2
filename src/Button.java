0c2af0a30dd3863aeec500c594cd11c89e1e0f58
Daantuk <daantuk@gmail.com>
2021-11-19 16:50:28 +0100

    Comment Button

--- src/Button.java
+++ src/Button.java
@@ -5,15 +5,22 @@ public class Button {
 
     private int inputPin;
 
+
     public Button(int inputPin) {
         this.inputPin = inputPin;
         BoeBot.setMode(this.inputPin, PinMode.Input);
     }
 
+    /**
+     * checks if button is pressed
+     */
     public boolean isPressed() {
         return !BoeBot.digitalRead(inputPin);
     }
 
+    /**
+     * method for interface in the future
+     */
     public void update(){
 
     }
