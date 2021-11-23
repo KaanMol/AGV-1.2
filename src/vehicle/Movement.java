9d23f9d3d3cc600055a11b9d9e94bd10aa116dc3
Momin2004 <m.musa@student.avans.nl>
2021-11-23 22:31:37 +0100

    Created a POC of the new movement system with slow acceleration

--- src/vehicle/Movement.java
+++ src/vehicle/Movement.java
@@ -1,45 +1,107 @@
 package vehicle;
 
+import TI.Timer;
 import hardware.Motor;
+import interfaces.MovementUpdater;
 import interfaces.Updatable;
 import common.Config;
 
 import java.util.ArrayList;
 
 public class Movement implements interfaces.hardware.Movement, Updatable {
+    public static final int NEUTRAL = 0;
+    public static final int FORWARD = 1;
+    public static final int RIGHT = 2;
+    public static final int BACKWARD = 3;
+    public static final int LEFT = 4;
 
     private Motor rightServo;
     private Motor leftServo;
+    private int currentHeading = 0;
+    private int leftMotorSpeed = 0;
+    private int rightMotorSpeed = 0;
+    private MovementUpdater callback;
+    private boolean isAccelerating = false;
+    private Timer timer;
 
-    public Movement() {
+    public Movement(MovementUpdater callback) {
+        this.callback = callback;
+        this.timer = new Timer(0);
         this.rightServo = new Motor(Config.rightServoPin);
         this.leftServo = new Motor(Config.leftServoPin);
     }
 
+    public int getHeading() {
+        return this.currentHeading;
+    }
+
     public void forward() {
+        this.isAccelerating = true;
+        this.rightMotorSpeed = 0;
+        this.leftMotorSpeed = 0;
+        this.timer.setInterval(250);
+        this.currentHeading = Movement.FORWARD;
+    }
+
+    public void instantForward() {
         this.rightServo.setSpeed(1300);
         this.leftServo.setSpeed(1700);
     }
 
     public void backwards() {
-        this.leftServo.setSpeed(1300);
-        this.rightServo.setSpeed(1700);
+        this.currentHeading = Movement.BACKWARD;
+//        this.rightServo.setSpeed(1700);
+//        this.leftServo.setSpeed(1300);
     }
 
     public void turnRight() {
-        this.leftServo.setSpeed(1300);
-        this.rightServo.setSpeed(1700);
+        this.currentHeading = Movement.BACKWARD;
+//        this.leftServo.setSpeed(1600);
+//        this.rightServo.setSpeed(1600);
     }
 
     public void turnLeft() {
-
+        this.currentHeading = Movement.BACKWARD;
+//        this.leftServo.setSpeed(1400);
+//        this.rightServo.setSpeed(1400);
     }
 
     public void update() {
+        if (this.timer.timeout() == false) {
+            return;
+        }
+//        if (this.currentHeading == Movement.FORWARD) {
+//            this.rightMotorSpeed = -200;
+//            this.leftMotorSpeed = 200;
+//        } else if (this.currentHeading == Movement.BACKWARD) {
+//            this.rightMotorSpeed = 200;
+//            this.leftMotorSpeed = -200;
+//        } else if (this.currentHeading == Movement.RIGHT) {
+//            this.rightMotorSpeed = 100;
+//            this.leftMotorSpeed = 100;
+//        } else if (this.currentHeading == Movement.LEFT) {
+//            this.rightMotorSpeed = -100;
+//            this.leftMotorSpeed = -100;
+//        } else {
+//            this.rightMotorSpeed = 0;
+//            this.leftMotorSpeed = 0;
+//        }
+        if (this.isAccelerating) {
+            System.out.println(this.leftMotorSpeed);
+            if (this.leftMotorSpeed < 200 && this.rightMotorSpeed > -200) {
+                this.leftMotorSpeed += 10;
+                this.rightMotorSpeed -= 10;
+            } else {
+                this.isAccelerating = false;
+            }
 
+
+
+
+            this.rightServo.setSpeed(1500 + this.rightMotorSpeed);
+            this.leftServo.setSpeed(1500 + this.leftMotorSpeed);
+        }
+
+        this.timer.mark();
     }
-
-//    public getTestResults(ArrayList<String> expected) {
-//        return this.
-//    }
 }
