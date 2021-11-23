dd59a3cc99a4be868fd24d9825e952b1dbd4e16f
Daantuk <daantuk@gmail.com>
2021-11-23 21:00:34 +0100

    Comments NeoPixel

--- src/Led.java
+++ src/Led.java
@@ -4,6 +4,9 @@ import java.awt.*;
 
 public class Led {
 
+    /**
+     * the pins for the NeoPixels
+     */
     public static final int bottomRight = 0;
     public static final int bottomMiddle = 1;
     public static final int bottomLeft = 2;
@@ -13,16 +16,26 @@ public class Led {
 
     private int inputPin;
 
+    /**
+     * @param inputPin The pin that the NeoPixel is connected to
+     */
     public Led(int inputPin) {
         this.inputPin = inputPin;
         BoeBot.setMode(inputPin, PinMode.Output);
     }
 
+    /**
+     * sets color for NeoPixel
+     * @param color Color for the NeoPixel
+     */
     public void setColor(Color color) {
         BoeBot.rgbSet(this.inputPin, color);
         BoeBot.rgbShow();
     }
 
+    /**
+     * turns off NeoPixel
+     */
     public void off() {
         BoeBot.rgbSet(this.inputPin, Color.black);
         BoeBot.rgbShow();
