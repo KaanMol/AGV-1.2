78759482906d6ef50f246a57425897891d27c189
Kaan <m.mollaoglu@student.avans.nl>
2021-11-23 19:03:16 +0100

    Add interface Movement and create a simple Unit Test POC

---
+++ src/test/facade/FakeMovement.java
@@ -0,0 +1,37 @@
+package test.facade;
+
+import interfaces.hardware.Movement;
+
+import java.util.ArrayList;
+
+public class FakeMovement implements Movement {
+    private UnitTest test;
+
+    public FakeMovement() {
+        this.test = new UnitTest(this.getClass().getSimpleName());
+    }
+
+    public void forward() {
+        this.test.pushResult("mv forward");
+    }
+
+    public void backwards() {
+        this.test.pushResult("mv backwards");
+    }
+
+    public void turnLeft() {
+        this.test.pushResult("mv left");
+    }
+
+    public void turnRight() {
+        this.test.pushResult("mv right");
+    }
+
+    public void runTest(ArrayList<String> expected) {
+        this.test.displayResults(expected);
+    }
+
+    public void clearTest() {
+        this.test.clearTest();
+    }
+}
