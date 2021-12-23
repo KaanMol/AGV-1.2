068bdc5b9d817ac199f5724bce0bbeb38ee47bed
Quincccy <ql.beerman@student.avans.nl>
2021-12-23 11:21:08 +0100

    brake lights

--- src/vehicle/DrivingLights.java
+++ src/vehicle/DrivingLights.java
@@ -9,9 +9,15 @@ import java.awt.*;
 public class DrivingLights {
     private Led topLeft;
     private Led topRight;
+    private Led topMiddle;
     private Led bottomLeft;
     private Led bottomRight;
+    private Led bottomMiddle;
     private Direction direction;
+    private Color color;
+    private boolean frontLightsOn;
+    private boolean backLightsOn;
+    private boolean brakeLightsOn;
 
     /**
      * Initializes the LED's and direction.
@@ -19,19 +25,22 @@ public class DrivingLights {
     public DrivingLights() {
         this.topLeft = new Led(Led.topLeft);
         this.topRight = new Led(Led.topRight);
+        this.topMiddle = new Led(Led.topMiddle);
         this.bottomLeft = new Led(Led.bottomLeft);
         this.bottomRight = new Led(Led.bottomRight);
-        this.direction = Direction.NEUTRAL;
+        this.bottomMiddle = new Led(Led.bottomMiddle);
+        this.direction = Direction.LEFT;
+        this.frontLightsOn = false;
+        this.backLightsOn = false;
+        this.brakeLightsOn = false;
         this.off();
     }
 
     /**
-     * Returns if the given direction is neutral and otherwise changes the
-     * direction attribute in the given direction.
      * @param direction - The direction that the Bot is currently heading.
      */
     public void start(Direction direction) {
-        if (this.direction == direction) {
+        if(this.direction == direction){
             return;
         }
         this.direction = direction;
@@ -43,12 +52,31 @@ public class DrivingLights {
      */
     public void on() {
         if (this.direction == Direction.FORWARD) {
+            if(this.backLightsOn == true || this.brakeLightsOn == true) {
+                this.off();
+            }
             topRight.setColor(Color.WHITE);
             topLeft.setColor(Color.WHITE);
+            topMiddle.setColor(Color.WHITE);
+            this.frontLightsOn = true;
         }
         else if (this.direction == Direction.BACKWARD) {
+            if(this.frontLightsOn == true || this.brakeLightsOn == true) {
+                this.off();
+            }
+            bottomRight.setColor(Color.getHSBColor(0, 1, 0.1f));
+            bottomLeft.setColor(Color.getHSBColor(0, 1, 0.1f));
+            bottomMiddle.setColor(Color.getHSBColor(0, 1, 0.1f));
+            this.backLightsOn = true;
+        }
+        else if (this.direction == Direction.NEUTRAL) {
+            if(this.frontLightsOn == true || this.backLightsOn == true) {
+                this.off();
+            }
             bottomRight.setColor(Color.RED);
             bottomLeft.setColor(Color.RED);
+            bottomMiddle.setColor(Color.RED);
+            this.brakeLightsOn = true;
         }
     }
 
@@ -66,7 +94,12 @@ public class DrivingLights {
     private void off() {
         this.topLeft.off();
         this.topRight.off();
+        this.topMiddle.off();
         this.bottomLeft.off();
         this.bottomRight.off();
+        this.bottomMiddle.off();
+        this.brakeLightsOn = false;
+        this.backLightsOn = false;
+        this.frontLightsOn = false;
     }
 }
