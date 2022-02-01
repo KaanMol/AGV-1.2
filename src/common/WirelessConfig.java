cfccdb8bdbb97b728e0665b7438edc26c3af6403
Momin2004 <m.musa@student.avans.nl>
2022-02-01 13:20:01 +0100

    Changed config look
    removed all comments and used catagories instead, so everything is easier to find

--- src/common/WirelessConfig.java
+++ src/common/WirelessConfig.java
@@ -2,6 +2,9 @@ package common;
 
 public class WirelessConfig {
 
+    /**
+     * Bluetooth
+     */
     public static final int forward = 119;
     public static final int backward = 115;
     public static final int left = 97;
@@ -11,4 +14,17 @@ public class WirelessConfig {
     public static final int gripper = 101;
     public static final int routeTransmissionStart = 82;
     public static final int routeTransmissionEnd = 59;
+
+    /**
+     * Remote
+     */
+    public static final int InfraredPin = 7;
+    public static final int remoteForward = 216;
+    public static final int remoteBackward = 217;
+    public static final int remoteLeft = 147;
+    public static final int remoteRight = 146;
+    public static final int remoteNeutral = 223;
+    public static final int remoteEmergencyStop = 149;
+    public static final int remoteControlTransfer = 151;
+    public static final int remoteGripper = 219;
 }
